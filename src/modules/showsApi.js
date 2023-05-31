64ab2c8953291f149b51ed3895ce69f55d19727c
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-31 01:57:44 +0100

    implement 2 API requests

--- src/modules/showsApi.js
+++ src/modules/showsApi.js
@@ -1,11 +1,24 @@
 import displayShows from './displayShows.js';
 
+
+// const fetchShows = async (id) => {
+//   await fetch(`https://api.tvmaze.com/shows/${id}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       displayShows(data);
+//     });
+// };
+
+// Make requests from the two API's
+
 const fetchShows = async (id) => {
   await fetch(`https://api.tvmaze.com/shows/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      displayShows(data);
-    });
+    .then((response) => response.json());
 };
+
+const fetchLikes = async () => {
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes')
+    .then(response => response.json());
+}
 
 export default fetchShows;
