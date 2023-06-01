05b73466d8790cd60bc3834e769bcb607456d0eb
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-02 01:27:18 +0500

    remove the eslint errors

--- src/modules/showsApi.js
+++ src/modules/showsApi.js
@@ -1,9 +1,8 @@
-const fetchData = async (link) =>
-  fetch(link).then((response) => response.json());
+const fetchData = async (link) => fetch(link).then((response) => response.json());
 
-export const fetchShows = fetchData("https://api.tvmaze.com/shows");
+export const fetchShows = fetchData('https://api.tvmaze.com/shows');
 export const fetchLikes = fetchData(
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes"
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes',
 );
 
 // function to handle Likes.
@@ -13,26 +12,24 @@ const addLike = (id) => {
   };
 
   fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes",
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json; charsert=UTF-8",
+        'Content-Type': 'application/json; charsert=UTF-8',
       },
       body: JSON.stringify(apiBody),
-    }
+    },
   )
-    .then(() =>
-      fetch(
-        "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes"
-      )
-    )
+    .then(() => fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fDeldDFM61dNTEWtzoPU/likes',
+    ))
     .then((response) => response.json())
     .then((data) => {
       // Live Updates the Like button
       let newId = id;
       newId -= 1;
-      const likeBtn = document.querySelectorAll(".likes-counter")[newId];
+      const likeBtn = document.querySelectorAll('.likes-counter')[newId];
       data.forEach((like) => {
         if (like.item_id === id) {
           likeBtn.innerHTML = `${like.likes} likes`;
