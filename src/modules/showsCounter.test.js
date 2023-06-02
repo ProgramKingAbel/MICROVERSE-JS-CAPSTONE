f40dd08435a8f33892c9412f8b9e4def06b9ebb9
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-02 18:51:30 +0500

    Resolved the readme file conflicts

--- src/modules/showsCounter.test.js
+++ src/modules/showsCounter.test.js
@@ -1,20 +1,24 @@
 /**
  * @jest-environment jsdom
  */
-import allShows from './showsCounter.js';
+import allShows from "./showsCounter.js";
 
 beforeEach(() => {
-  document.body.innerHTML = '<span class=\'total\'></span>';
+  document.body.innerHTML = "<span class='total'></span>";
 });
 afterEach(() => {
-  document.body.innerHTML = '';
+  document.body.innerHTML = "";
 });
 
-describe('Test Counter operations', () => {
-  test('add one item', () => {
-    const data = [{ id: 1, name: 'Show 1' }, { id: 2, name: 'Show 2' }, { id: 3, name: 'Show 3' }];
+describe("Test Counter operations", () => {
+  test("add one item", () => {
+    const data = [
+      { id: 1, name: "Show 1" },
+      { id: 2, name: "Show 2" },
+      { id: 3, name: "Show 3" },
+    ];
     allShows(data);
-    const container = document.querySelector('.total');
-    expect(container.innerHTML).toBe('3');
+    const container = document.querySelector(".total");
+    expect(container.innerHTML).toBe("3");
   });
 });
