58d50579c1fd199620dd5c9ec08c238eb66bb350
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-02 19:06:42 +0500

    Fix the license issue and aligned the navigation menu suggested by reviewer

--- src/modules/showsCounter.test.js
+++ src/modules/showsCounter.test.js
@@ -1,24 +1,24 @@
 /**
  * @jest-environment jsdom
  */
-import allShows from "./showsCounter.js";
+import allShows from './showsCounter.js';
 
 beforeEach(() => {
   document.body.innerHTML = "<span class='total'></span>";
 });
 afterEach(() => {
-  document.body.innerHTML = "";
+  document.body.innerHTML = '';
 });
 
-describe("Test Counter operations", () => {
-  test("add one item", () => {
+describe('Test Counter operations', () => {
+  test('add one item', () => {
     const data = [
-      { id: 1, name: "Show 1" },
-      { id: 2, name: "Show 2" },
-      { id: 3, name: "Show 3" },
+      { id: 1, name: 'Show 1' },
+      { id: 2, name: 'Show 2' },
+      { id: 3, name: 'Show 3' },
     ];
     allShows(data);
-    const container = document.querySelector(".total");
-    expect(container.innerHTML).toBe("3");
+    const container = document.querySelector('.total');
+    expect(container.innerHTML).toBe('3');
   });
 });
